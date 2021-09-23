f6222c030dc102b61dfd5ed1a50485584e8e76c6
Harry Winer <winer.harry@gmail.com>
2021-09-23 01:46:20 +0100

    moved functions to tools

--- server.js
+++ server.js
@@ -16,69 +16,13 @@ app.use(express.static(__dirname + "/public"));
 console.log("Listening on " + port);
 app.listen(port);
 
-function CountPlays(connection) {
-  return new Promise((resolve, reject) => {
-    var query = "SELECT COUNT(*) as playCount FROM plays;";
 
-    console.log(query);
-
-    connection.query(query, function (err, result) {
-      if (err) {
-        console.log("Count Plays error: " + err);
-
-        reject();
-      } else {
-        resolve(result[0].playCount);
-      }
-    });
-  });
-}
-
-function MostPlayed(connection, limit, offset) {
-  return new Promise((resolve, reject) => {
-    var query =
-      "select trackName, artistName, count(*) as times from plays where msPlayed >= 10000 group by trackName, artistName order by times desc LIMIT ? OFFSET ?;";
-    var inputs = [limit, offset];
-
-    query = connection.format(query, inputs);
-
-    connection.query(query, function (err, result) {
-      if (err) {
-        console.log("Most Played error: " + err);
-
-        reject();
-      } else {
-        resolve(result);
-      }
-    });
-  });
-}
-
-function PlayTime(connection, limit, offset) {
-  return new Promise((resolve, reject) => {
-    var query =
-      "select trackName, artistName, sum(msPlayed) as timeListened from plays group by trackname, artistName order by timeListened desc limit ? offset ?;";
-    var inputs = [limit, offset];
-
-    query = connection.format(query, inputs);
-
-    connection.query(query, function (err, result) {
-      if (err) {
-        console.log("Most Played error: " + err);
-
-        reject();
-      } else {
-        resolve(result);
-      }
-    });
-  });
-}
 
 app.get("/count-total-plays", function (req, res) {
   var dbPromise = tools.DBConnect("spotify");
 
   dbPromise.then((connection) => {
-    var countPromise = CountPlays(connection);
+    var countPromise = tools.CountPlays(connection);
 
     countPromise.then((count) => {
       res.send({
@@ -99,7 +43,7 @@ app.get("/most-played", function (req, res) {
   console.log("Limit: " + req.query.limit);
 
   dbPromise.then((connection) => {
-    var playPromise = MostPlayed(
+    var playPromise = tools.MostPlayed(
       connection,
       parseInt(req.query.limit),
       parseInt(req.query.offset)
@@ -124,7 +68,7 @@ app.get("/play-time", function (req, res) {
   console.log("Limit: " + req.query.limit);
 
   dbPromise.then((connection) => {
-    var playPromise = PlayTime(
+    var playPromise = tools.PlayTime(
       connection,
       parseInt(req.query.limit),
       parseInt(req.query.offset)
